9abe4bc331f2730e6353473d3cb4f1f7f8eedebd
Farhan Alam <115414440+FarhanAlam-Official@users.noreply.github.com>
2026-02-12 16:30:00 +0545

    feat: add TypeScript type exports

---
+++ frontend/types/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Type definition exports
+ */
+
+// Export all types as they are created
+export * from './auth.types'
+export * from './property.types'
+// export * from './property.types'
+// export * from './dashboard.types'
