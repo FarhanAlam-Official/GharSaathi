87091f3ad64f7b0e094600ec0a3c14769ad65bc2
Farhan Alam <115414440+FarhanAlam-Official@users.noreply.github.com>
2026-02-13 10:30:00 +0545

    feat: add services index for exports

---
+++ frontend/lib/services/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Service exports
+ */
+
+// Export all services as they are created
+export * from './auth.service'
+// export * from './property.service'
+// export * from './dashboard.service'
